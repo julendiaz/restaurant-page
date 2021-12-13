3c6328e74cc7690e28b6d7bd0f47fe069ab4608a
julendiaz <iamjulendiaz@gmail.com>
2021-12-13 21:30:41 +0100

    Add main content to the homepage with the DOM

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,15 @@ import _ from "lodash";
 const content = document.querySelector("#content");
 
 function home() {
+
+    //----------------NAVBAR-------------------//
     const navbar = document.createElement("div");
 
     navbar.classList.add("Navbar"); 
 
     // Navbar Left elements
-    const navLinks = document.createElement("nav");
-    const navContainer = document.createElement("ul");
+    const navLeft = document.createElement("nav");
+    const navLeft__links = document.createElement("ul");
     const linkHome_Container = document.createElement("li");
     const linkHome = document.createElement("a");
     const linkMenu_Container = document.createElement("li");
@@ -17,14 +19,14 @@ function home() {
     const linkChef_Container = document.createElement("li");
     const linkChef= document.createElement("a");
     // add Classes
-    navLinks.classList.add("nav__links"); 
-    navLinks.id = "nav__left"; 
+    navLeft.classList.add("nav__links"); 
+    navLeft.id = "nav__left"; 
     linkHome.innerHTML = "Home";
     linkMenu.innerHTML = "Menu";
     linkChef.innerHTML = "Chef";
     // append elements
-    navLinks.appendChild(navContainer);
-    navContainer.appendChild(linkHome_Container, linkMenu_Container, linkChef_Container);
+    navLeft.appendChild(navLeft__links);
+    navLeft__links.appendChild(linkHome_Container, linkMenu_Container, linkChef_Container);
     linkHome_Container.appendChild(linkHome);
     linkMenu_Container.appendChild(linkMenu);
     linkChef_Container.appendChild(linkChef);
@@ -36,7 +38,7 @@ function home() {
 
     // Navbar Right Elements
     const navRight = document.createElement("nav");
-    const navRight__Container = document.createElement("ul");
+    const navRight__links = document.createElement("ul");
     const linkReservations_Container = document.createElement("li");
     const linkReservations = document.createElement("a");
     const linkLocation_Container = document.createElement("li");
@@ -49,11 +51,28 @@ function home() {
     linkLocation.innerHTML = "Location";
 
     // append elements
-    navRight.appendChild(navRight__Container);
-    navRight__Container.appendChild(linkReservations_Container, linkLocation_Container);
+    navRight.appendChild(navRight__links);
+    navRight__links.appendChild(linkReservations_Container, linkLocation_Container);
     linkReservations_Container.appendChild(linkReservations);
     linkLocation_Container.appendChild(linkLocation);
 
     // Append the big elements to the Navbar
-    navbar.appendChild(navLinks, logo, navRight)
+    navbar.appendChild(navLeft, logo, navRight);
+
+    //-----------MAIN CONTENT-------------------// 
+    const mainContent = document.createElement("div");
+    const locationTitle = document.createElement("h1");
+    const ramenImg = document.createElement("img");
+    const scheduleTitle = document.createElement("h1");
+
+    mainContent.id = "main-content";
+    locationTitle.innerHTML = "Eberhardstrasse 47<br> Stuttgart";
+    ramenImg.src = "/src/assets/kaido-ramen.jpeg";
+    ramenImg.alt = "Ramen Image";
+    scheduleTitle.innerHTML = "12:00 to 0:00<br /> Every day";
+
+    mainContent.appendChild(locationTitle, ramenImg, scheduleTitle);
+
+
+
 }
